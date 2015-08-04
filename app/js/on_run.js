54e91c378a23a684bc59e4b19c9ebee49024bd90
Craig McMurray <craig.mcmurray@dcsg.com>
2015-08-04 10:56:09 -0400

    Minor changes.

--- app/js/on_run.js
+++ app/js/on_run.js
@@ -6,13 +6,17 @@
  */
 function OnRun($rootScope, AppSettings) {
 	//Set default value for Booleans
-	$rootScope.isMobile = true;
-	$rootScope.fabDirection = 'up';
+	$rootScope.fab = {
+		class: 'md-fab-bottom-left',
+		direction: 'up'
+	};
 	//Check viewport size
 	if (Modernizr.mq('only all and (min-width: 1025px)')) {
-		//We are on desktop, flip the Booleans
-		$rootScope.isMobile = false;
-		$rootScope.fabDirection = 'down';
+		//We are on desktop, change the properties
+		$rootScope.fab = {
+			class: 'md-fab-top-left',
+			direction: 'down'
+		};
 	}
 	//Set the Page Title
 	$rootScope.pageTitle = AppSettings.appTitle;
